e16c5c619dd11405d0603b73d5939162d96f4768
Javi <bisistems@gmail.com>
2020-11-04 18:49:02 +0100

    Fase1

---
+++ variables/VariablesApp.java
@@ -0,0 +1,26 @@
+package variables;
+
+public class VariablesApp {
+	 
+
+	public static void main(String[] args) {
+		
+		/************************** Fase 1 *****************************************/
+		
+		String nom = "javi";
+		String cognom1 = "benito";
+		String cognom2 = "martinez";
+		
+		int dia = 22;
+		int mes = 07;
+		int any = 1988;
+		
+		System.out.print("Nombre: " +cognom1+ " " +cognom2+ ", " +nom+ "\n");
+		System.out.print("Edad: " +dia+ "/" +mes+ "/" +any+ "\n");
+		
+		
+		
+
+	}
+
+}
