d99a202b398a5512593a000f687b09c2b13cccf5
Javi <bisistems@gmail.com>
2020-11-04 19:25:41 +0100

    Fase3

--- variables/VariablesApp.java
+++ variables/VariablesApp.java
@@ -21,7 +21,7 @@ public class VariablesApp {
 		
 		/************************** Fase 2 *****************************************/
 		
-		final int traspas = 1948;
+		int traspas = 1948;
 		int temps = 4;
 		int anys = 0;
 		
@@ -35,7 +35,42 @@ public class VariablesApp {
 		System.out.print("\nAnys de traspas en " +traspas+ " y " +any+ ": " +anys);
 		
 		
+		/*************************** Fase 3 *************************************************/
 		
+		String resultado = "";
+		int calc = 0;
+		boolean cert = false;
+		String frase1 = "\nEl any " +any+ " es de traspas";
+		String frase2 = "\nEl any " +any+ " no es de traspas";
+		
+		System.out.print("\n\n");
+		System.out.print("Els anys de traspas desde " +traspas+ " son:");
+		
+		for(var i = traspas; i < any; i++) {
+			
+			if(i % 400 == 0 || (i % 4 == 0 && i % 100 != 0) ){
+				
+				calc = i + temps;
+				
+				resultado += " " + calc;
+	
+			}
+			
+		}
+		
+		System.out.print(resultado);
+		
+		System.out.print("\n");
+		
+		if(any % 400 == 0 || (any % 4 == 0 && any % 100 != 0)) {
+			cert = true;
+			System.out.print(frase1);
+			
+		} else {
+			cert = false;
+			System.out.print(frase2);
+			
+		}
 
 	}
 
